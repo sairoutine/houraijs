65c7fc9e16e0fed034a7e4bfeecf9a1bf3b25ffb
sairoutine <sairoutine@gmail.com>
2016-11-03 15:32:33 +0900

    add: PMD class

---
+++ src/js/PMD.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var PMD = function() {
+
+};
+
+PMD.prototype.setup = function () {
+
+};
+
+PMD.prototype.loadImages = function (image_base_url, callback) {
+
+	return callback();
+};
+
+
+
+
+module.exports = PMD;
