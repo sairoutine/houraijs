c50475cd123738c8c6cdb5115efc12e608b18142
sairoutine <sairoutine@gmail.com>
2016-11-12 23:05:52 +0900

    add: property name in PMD class

--- src/js/PMD.js
+++ src/js/PMD.js
@@ -1,7 +1,49 @@
 'use strict';
 
 var PMD = function() {
+	this.header = null;
+	this.englishHeader = null;
+	this.vertexCount = null;
+	this.vertexIndexCount = null;
+	this.materialCount = null;
+	this.boneCount = null;
+	this.ikCount = null;
+	this.faceCount = null;
+	this.faceDisplayCount = null;
+	this.boneFrameNameCount = null;
+	this.boneDisplayCount = null;
+	this.toonTextureCount = null;
+	this.rigidBodyCount = null;
+	this.jointCount = null;
 
+	this.vertices = [];
+	this.vertexIndices = []
+	this.materials = [];
+	this.bones = [];
+	this.iks = [];
+	this.faces = [];
+	this.faceDisplays = [];
+	this.boneFrameNames = [];
+	this.boneDisplays = [];
+	this.englishBoneNames = [];
+	this.englishFaceNames = [];
+	this.englishBoneFrameNames = [];
+	this.toonTextures = [];
+	this.rigidBodies = [];
+	this.joints = [];
+
+	this.bonesHash = {};
+	this.facesHash = {};
+
+	this.images = [];
+	this.toonImages = [];
+	this.sphereImages = [];
+
+	this.centerBone = {};
+	this.leftFootBone = {};
+	this.rightFootBone = {};
+	this.leftEyeBone = {};
+	this.rightEyeBone = {};
 };
 
 PMD.prototype.setup = function () {
