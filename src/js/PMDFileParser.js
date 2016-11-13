462818bb02cc6154af6584c664f9fe1c038a1d62
sairoutine <sairoutine@gmail.com>
2016-11-13 16:13:01 +0900

    implement PMDFileParser

--- src/js/PMDFileParser.js
+++ src/js/PMDFileParser.js
@@ -7,10 +7,210 @@ var HEADER_STRUCTURE = {
 	comment: {type: 'char', isArray: true, size: 256}
 };
 
+var VERTICES_STRUCTURE = {
+	count: {type: 'uint32'},
+	vertices: {type: 'object', isArray: true, size: 'count'}
+};
+
+var VERTEX_STRUCTURE = {
+	position: {type: 'float', isArray: true, size: 3},
+	normal: {type: 'float', isArray: true, size: 3},
+	uv: {type: 'float', isArray: true, size: 2},
+	boneIndices: {type: 'uint16', isArray: true, size: 2},
+	boneWeight: {type: 'uint8'},
+	edgeFlag: {type: 'uint8'}
+};
+
+var VERTEX_INDICES_STRUCTURE = {
+	count: {type: 'uint32'},
+	indices: {type: 'object', isArray: true, size: 'count'}
+};
+
+var VERTEX_INDEX_STRUCTURE = {
+	index: {type: 'uint16'}
+};
+
+
+var MATERIALS_STRUCTURE = {
+	count: {type: 'uint32'},
+	materials: {type: 'object', isArray: true, size: 'count'}
+};
+
+var MATERIAL_STRUCTURE = {
+	color: {type: 'float', isArray: true, size: 4},
+	specularity: {type: 'float'},
+	specularColor: {type: 'float', isArray: true, size: 3},
+	mirrorColor: {type: 'float', isArray: true, size: 3},
+	tuneIndex: {type: 'uint8'},
+	edgeFlag: {type: 'uint8'},
+	vertexCount: {type: 'uint32'},
+	fileName: {type: 'char', isArray: true, size: 20}
+};
+
+var BONES_STRUCTURE = {
+	count: {type: 'uint16'},
+	bones: {type: 'object', isArray: true, size: 'count'}
+};
+
+var BONE_STRUCTURE = {
+	name: {type: 'strings', isArray: true, size: 20},
+	parentIndex: {type: 'uint16'},
+	tailIndex: {type: 'uint16'},
+	type: {type: 'uint8'},
+	ikIndex: {type: 'uint16'},
+	position: {type: 'float', isArray: true, size: 3}
+};
+
+var IKS_STRUCTURE = {
+	count: {type: 'uint16'},
+	iks: {type: 'object', isArray: true, size: 'count'}
+};
+
+var IK_STRUCTURE = {
+	index: {type: 'uint16'},
+	targetBoneIndex: {type: 'uint16'},
+	chainLength: {type: 'uint8'},
+	iteration: {type: 'uint16'},
+	limitation: {type: 'float'},
+	childBoneIndices: {type: 'uint16', isArray: true, size: 'chainLength'}
+};
+
+var FACES_STRUCTURE = {
+	count: {type: 'uint16'},
+	faces: {type: 'object', isArray: true, size: 'count'}
+};
+
+var FACE_STRUCTURE = {
+	name: {type: 'strings', isArray: true, size: 20},
+	vertexCount: {type: 'uint32'},
+	type: {type: 'uint8'},
+	vertices: {type: 'object', isArray: true, size: 'vertexCount'}
+};
+
+var FACE_VERTEX_STRUCTURE = {
+	index: {type: 'uint32'},
+	position: {type: 'float', isArray: true, size: 3}
+};
+
+var FACE_DISPLAYS_STRUCTURE = {
+	count: {type: 'uint8'},
+	indices: {type: 'object', isArray: true, size: 'count'}
+};
+
+var FACE_DISPLAY_STRUCTURE = {
+	index: {type: 'uint16'}
+};
+
+var BONE_FRAME_NAMES_STRUCTURE = {
+	count: {type: 'uint8'},
+	names: {type: 'object', isArray: true, size: 'count'}
+};
+
+var BONE_FRAME_NAME_STRUCTURE = {
+	name: {type: 'strings', isArray: true, size: 50}
+};
+
+var BONE_DISPLAYS_STRUCTURE = {
+	count: {type: 'uint32'},
+	displays: {type: 'object', isArray: true, size: 'count'}
+};
+
+var BONE_DISPLAY_STRUCTURE = {
+	index: {type: 'uint16'},
+	frameIndex: {type: 'uint8'}
+};
+
+var ENGLISH_HEADER_STRUCTURE = {
+	compatibility: {type: 'uint8'},
+	modelName: {type: 'char', isArray: true, size: 20},
+	comment: {type: 'char', isArray: true, size: 256}
+};
+
+var ENGLISH_BONE_NAME_STRUCTURE = {
+	name: {type: 'char', isArray: true, size: 20}
+};
+
+var ENGLISH_FACE_NAME_STRUCTURE = {
+	name: {type: 'char', isArray: true, size: 20}
+};
+
+var ENGLISH_BONE_FRAME_NAME_STRUCTURE = {
+	name: {type: 'char', isArray: true, size: 50}
+};
+
+var TOON_TEXTURE_STRUCTURE = {
+	fileName: {type: 'char', isArray: true, size: 100}
+};
+
+var RIGID_BODIES_STRUCTURE = {
+	count: {type: 'uint32'},
+	bodies: {type: 'object', isArray: true, size: 'count'}
+};
+
+var RIGID_BODY_STRUCTURE = {
+	name: {type: 'strings', isArray: true, size: 20},
+	boneIndex: {type: 'uint16'},
+	groupIndex: {type: 'uint8'},
+	groupTarget: {type: 'uint16'},
+	shapeType: {type: 'uint8'},
+	width: {type: 'float'},
+	height: {type: 'float'},
+	depth: {type: 'float'},
+	position: {type: 'float', isArray: true, size: 3},
+	rotation: {type: 'float', isArray: true, size: 3},
+	weight: {type: 'float'},
+	positionDim: {type: 'float'},
+	rotationDim: {type: 'float'},
+	recoil: {type: 'float'},
+	friction: {type: 'float'},
+	type: {type: 'uint8'}
+};
+
+var JOINTS_STRUCTURE = {
+	count: {type: 'uint32'},
+	joints: {type: 'object', isArray: true, size: 'count'}
+};
+
+var JOINT_STRUCTURE = {
+	name: {type: 'strings', isArray: true, size: 20},
+	rigidBody1: {type: 'uint32'},
+	rigidBody2: {type: 'uint32'},
+	position: {type: 'float', isArray: true, size: 3},
+	rotation: {type: 'float', isArray: true, size: 3},
+	translationLimitation1: {type: 'float', isArray: true, size: 3},
+	translationLimitation2: {type: 'float', isArray: true, size: 3},
+	rotationLimitation1: {type: 'float', isArray: true, size: 3},
+	rotationLimitation2: {type: 'float', isArray: true, size: 3},
+	springPosition: {type: 'float', isArray: true, size: 3},
+	springRotation: {type: 'float', isArray: true, size: 3}
+};
+
 
 
 var PMD = require('./PMD');
-var PMDHeader = require('./PMDHeader');
+var PMDHeader               = require('./PMD/Header');
+var PMDVertex               = require('./PMD/Vertex');
+var PMDVertexIndex          = require('./PMD/VertexIndex');
+var PMDMaterial             = require('./PMD/Material');
+var PMDBone                 = require('./PMD/Bone');
+var PMDIK                   = require('./PMD/Ik');
+var PMDFace                 = require('./PMD/Face');
+var PMDFaceVertex           = require('./PMD/FaceVertex');
+var PMDFaceDisplay          = require('./PMD/FaceDisplay');
+var PMDBoneFrameName        = require('./PMD/BoneFrameName');
+var PMDBoneDisplay          = require('./PMD/BoneDisplay');
+var PMDEnglishHeader        = require('./PMD/EnglishHeader');
+var PMDEnglishBoneName      = require('./PMD/EnglishBoneName');
+var PMDEnglishBoneFrameName = require('./PMD/EnglishBoneFrameName');
+var PMDEnglishFaceName      = require('./PMD/EnglishFaceName');
+var PMDToonTexture          = require('./PMD/ToonTexture');
+var PMDRigidBody            = require('./PMD/RigidBody');
+var PMDJoint                = require('./PMD/Joint');
+
+
+
+
+
 
 var PMDFileParser = function(buffer) {
 	this.uint8 = new Uint8Array(buffer);
@@ -28,8 +228,25 @@ PMDFileParser.prototype.parse = function () {
 	this.offset = 0;
 	var pmd = new PMD();
 
-	// TODO: parser
 	this._parseHeader(pmd);
+	this._parseVertices(pmd);
+	this._parseVertexIndices(pmd);
+	this._parseMaterials(pmd);
+	this._parseBones(pmd);
+	this._parseIKs(pmd);
+	this._parseFaces(pmd);
+	this._parseFaceDisplays(pmd);
+	this._parseBoneFrameNames(pmd);
+	this._parseBoneDisplays(pmd);
+	this._parseEnglishHeader(pmd);
+	if(this.englishCompatibility) {
+		this._parseEnglishBoneNames(pmd);
+		this._parseEnglishFaceNames(pmd);
+		this._parseEnglishBoneFrameNames(pmd);
+	}
+	this._parseToonTextures(pmd);
+	this._parseRigidBodies(pmd);
+	this._parseJoints(pmd);
 
 	return pmd;
 };
@@ -38,6 +255,320 @@ PMDFileParser.prototype._parseHeader = function(pmd) {
 	pmd.header = new PMDHeader();
 	this._parseObject(pmd.header, HEADER_STRUCTURE);
 };
+
+PMDFileParser.prototype._parseVertices = function(pmd) {
+	var structure = VERTICES_STRUCTURE;
+	pmd.vertexCount = this._getValue(structure.count, this.offset);
+	this.offset += this._sizeof(structure.count);
+
+	pmd.vertices.length = 0;
+	for(var i = 0; i < pmd.vertexCount; i++) {
+		this._parseVertex(pmd, i);
+	}
+};
+
+
+PMDFileParser.prototype._parseVertex = function(pmd, i) {
+	var structure = VERTEX_STRUCTURE;
+	var vertex = new PMDVertex(i);
+	this._parseObject(vertex, structure);
+	pmd.vertices[i] = vertex;
+};
+
+
+PMDFileParser.prototype._parseVertexIndices = function(pmd) {
+	var structure = VERTEX_INDICES_STRUCTURE;
+	pmd.vertexIndexCount = this._getValue(structure.count, this.offset);
+	this.offset += this._sizeof(structure.count);
+
+	pmd.vertexIndices.length = 0;
+	for(var i = 0; i < pmd.vertexIndexCount; i++) {
+		this._parseVertexIndex(pmd, i);
+	}
+};
+
+
+PMDFileParser.prototype._parseVertexIndex = function(pmd, i) {
+	var structure = VERTEX_INDEX_STRUCTURE;
+	var v = new PMDVertexIndex(i);
+	this._parseObject(v, structure);
+	pmd.vertexIndices[i] = v;
+};
+
+
+PMDFileParser.prototype._parseMaterials = function(pmd) {
+	var structure = MATERIALS_STRUCTURE;
+	pmd.materialCount = this._getValue(structure.count, this.offset);
+	this.offset += this._sizeof(structure.count);
+
+	pmd.materials.length = 0;
+	for(var i = 0; i < pmd.materialCount; i++) {
+		this._parseMaterial(pmd, i);
+	}
+};
+
+
+PMDFileParser.prototype._parseMaterial = function(pmd, i) {
+	var structure = MATERIAL_STRUCTURE;
+	var material = new PMDMaterial(i);
+	this._parseObject(material, structure);
+	pmd.materials[i] = material;
+};
+
+
+PMDFileParser.prototype._parseBones = function(pmd) {
+	var structure = BONES_STRUCTURE;
+	pmd.boneCount = this._getValue(structure.count, this.offset);
+	this.offset += this._sizeof(structure.count);
+
+	pmd.bones.length = 0;
+	for(var i = 0; i < pmd.boneCount; i++) {
+		this._parseBone(pmd, i);
+	}
+};
+
+PMDFileParser.prototype._parseBone = function(pmd, i) {
+	var structure = BONE_STRUCTURE;
+	var bone = new PMDBone(i);
+	this._parseObject(bone, structure);
+	pmd.bones[i] = bone;
+};
+
+PMDFileParser.prototype._parseIKs = function(pmd) {
+	var structure = IKS_STRUCTURE;
+	pmd.ikCount = this._getValue(structure.count, this.offset);
+	this.offset += this._sizeof(structure.count);
+
+	pmd.iks.length = 0;
+	for(var i = 0; i < pmd.ikCount; i++) {
+		this._parseIK(pmd, i);
+	}
+};
+
+
+PMDFileParser.prototype._parseIK = function(pmd, i) {
+	var structure = IK_STRUCTURE;
+	var ik = new PMDIK(i);
+	for(var key in structure) {
+		// childBoneIndices are variable length array
+		if(key == 'childBoneIndices') continue;
+
+		ik[key] = this._getValue(structure[key], this.offset);
+		this.offset += this._sizeof(structure[key]);
+	}
+
+
+	// TODO: refactor
+	ik.childBoneIndices = [];
+	var size = this._sizeofScalar(structure.childBoneIndices);
+	for(var j = 0; j < ik.chainLength; j++) {
+		ik.childBoneIndices[j] = this._getValueScalar(structure.childBoneIndices, this.offset);
+		this.offset += size;
+	}
+	pmd.iks[i] = ik;
+};
+
+
+PMDFileParser.prototype._parseFaces = function(pmd) {
+	var structure = FACES_STRUCTURE;
+	pmd.faceCount = this._getValue(structure.count, this.offset);
+	this.offset += this._sizeof(structure.count);
+
+	pmd.faces.length = 0;
+	for(var i = 0; i < pmd.faceCount; i++) {
+		this._parseFace(pmd, i);
+	}
+};
+
+
+PMDFileParser.prototype._parseFace = function(pmd, i) {
+	var structure = FACE_STRUCTURE;
+	var face = new PMDFace(i);
+
+	for(var key in structure) {
+		// variable length array
+		if(key === 'vertices') continue;
+
+		face[key] = this._getValue(structure[key], this.offset);
+		this.offset += this._sizeof(structure[key]);
+	}
+
+	face.vertices = [];
+	for(var j = 0; j < face.vertexCount; j++) {
+		this._parseFaceVertex(face, j, face.type);
+	}
+	pmd.faces[i] = face;
+};
+
+
+PMDFileParser.prototype._parseFaceVertex = function(face, i, type) {
+	var structure = FACE_VERTEX_STRUCTURE;
+	var face_vertex = new PMDFaceVertex(i, type);
+	this._parseObject(face_vertex, structure);
+	face.vertices[i] = face_vertex;
+};
+
+
+PMDFileParser.prototype._parseFaceDisplays = function(pmd) {
+	var structure = FACE_DISPLAYS_STRUCTURE;
+	pmd.faceDisplayCount = this._getValue(structure.count, this.offset);
+	this.offset += this._sizeof(structure.count);
+
+	pmd.faceDisplays.length = 0;
+	for(var i = 0; i < pmd.faceDisplayCount; i++) {
+		this._parseFaceDisplay(pmd, i);
+	}
+};
+
+PMDFileParser.prototype._parseFaceDisplay = function(pmd, i) {
+	var structure = FACE_DISPLAY_STRUCTURE;
+	var face_display = new PMDFaceDisplay(i);
+	this._parseObject(face_display, structure);
+	pmd.faceDisplays[i] = face_display;
+};
+
+
+PMDFileParser.prototype._parseBoneFrameNames = function(pmd) {
+	var structure = BONE_FRAME_NAMES_STRUCTURE;
+	pmd.boneFrameNameCount = this._getValue(structure.count, this.offset);
+	this.offset += this._sizeof(structure.count);
+
+	pmd.boneFrameNames.length = 0;
+	for(var i = 0; i < pmd.boneFrameNameCount; i++) {
+		this._parseBoneFrameName(pmd, i);
+	}
+};
+
+
+PMDFileParser.prototype._parseBoneFrameName = function(pmd, i) {
+	var structure = BONE_FRAME_NAME_STRUCTURE;
+	var bone_frame_name = new PMDBoneFrameName(i);
+	this._parseObject(bone_frame_name, structure);
+	pmd.boneFrameNames[i] = bone_frame_name;
+};
+
+
+PMDFileParser.prototype._parseBoneDisplays = function(pmd) {
+	var structure = BONE_DISPLAYS_STRUCTURE;
+	pmd.boneDisplayCount = this._getValue(structure.count, this.offset);
+	this.offset += this._sizeof(structure.count);
+
+	pmd.boneDisplays.length = 0;
+	for(var i = 0; i < pmd.boneDisplayCount; i++) {
+		this._parseBoneDisplay(pmd, i);
+	}
+};
+
+
+PMDFileParser.prototype._parseBoneDisplay = function(pmd, i) {
+	var structure = BONE_DISPLAY_STRUCTURE;
+	var bone_display = new PMDBoneDisplay(i);
+	this._parseObject(bone_display, structure);
+	pmd.boneDisplays[i] = bone_display;
+};
+
+
+PMDFileParser.prototype._parseEnglishHeader = function(pmd) {
+	var structure = ENGLISH_HEADER_STRUCTURE;
+	pmd.englishHeader = new PMDEnglishHeader();
+	this._parseObject(pmd.englishHeader, structure);
+
+	if(pmd.englishHeader.compatibility === 0) {
+		this.offset -= this._sizeofObject(structure);
+		this.offset += this._sizeof(structure.compatibility);
+		this.englishCompatibility = false;
+	}
+	else {
+		this.englishCompatibility = true;
+	}
+};
+
+
+PMDFileParser.prototype._parseEnglishBoneNames = function(pmd) {
+	var structure = ENGLISH_BONE_NAME_STRUCTURE;
+	pmd.englishBoneNames.length = 0;
+
+	for(var i = 0; i < pmd.boneCount; i++) {
+		var bone_name = new PMDEnglishBoneName(i);
+		this._parseObject(bone_name, structure);
+		pmd.englishBoneNames[i] = bone_name;
+	}
+};
+
+
+PMDFileParser.prototype._parseEnglishFaceNames = function(pmd) {
+	var structure = ENGLISH_FACE_NAME_STRUCTURE;
+	pmd.englishFaceNames.length = 0;
+	for(var i = 0; i < pmd.faceCount-1; i++) {
+		var e_face_name = new PMDEnglishFaceName(i);
+		this._parseObject(e_face_name, structure);
+		pmd.englishFaceNames[i] = e_face_name;
+	}
+};
+
+
+PMDFileParser.prototype._parseEnglishBoneFrameNames = function(pmd) {
+	var structure = ENGLISH_BONE_FRAME_NAME_STRUCTURE;
+	pmd.englishBoneFrameNames.length = 0;
+	for(var i = 0; i < pmd.boneFrameNameCount; i++) {
+		var e_bone_frame_name = new PMDEnglishBoneFrameName(i);
+		this._parseObject(e_bone_frame_name, structure);
+		pmd.englishBoneFrameNames[i] = e_bone_frame_name;
+	}
+};
+
+
+PMDFileParser.prototype._parseToonTextures = function(pmd) {
+	var structure = TOON_TEXTURE_STRUCTURE;
+	pmd.toonTextureCount = 10; // TODO: const
+	pmd.toonTextures.length = 0;
+	for(var i = 0; i < pmd.toonTextureCount; i++) {
+		var toon_texture = new PMDToonTexture(i);
+		this._parseObject(toon_texture, structure);
+		pmd.toonTextures[i] = toon_texture;
+	}
+};
+
+
+PMDFileParser.prototype._parseRigidBodies = function(pmd) {
+	var structure = RIGID_BODIES_STRUCTURE;
+	pmd.rigidBodyCount = this._getValue(structure.count, this.offset);
+	this.offset += this._sizeof(structure.count);
+
+	pmd.rigidBodies.length = 0;
+	for(var i = 0; i < pmd.rigidBodyCount; i++) {
+		this._parseRigidBody(pmd, i);
+	}
+};
+
+
+PMDFileParser.prototype._parseRigidBody = function(pmd, i) {
+	var structure = RIGID_BODY_STRUCTURE;
+	var rigid_body = new PMDRigidBody(i);
+	this._parseObject(rigid_body, structure);
+	pmd.rigidBodies[i] = rigid_body;
+};
+
+
+PMDFileParser.prototype._parseJoints = function(pmd) {
+	var structure = JOINTS_STRUCTURE;
+	pmd.jointCount = this._getValue(structure.count, this.offset);
+	this.offset += this._sizeof(structure.count);
+
+	pmd.joints.length = 0;
+	for(var i = 0; i < pmd.jointCount; i++) {
+		this._parseJoint(pmd, i);
+	}
+};
+
+
+PMDFileParser.prototype._parseJoint = function(pmd, i) {
+	var structure = JOINT_STRUCTURE;
+	var joint = new PMDJoint(i);
+	this._parseObject(joint, structure);
+	pmd.joints[i] = joint;
+};
+
 
 PMDFileParser.prototype._parseObject = function(obj, structure) {
 	var o = this.offset;
