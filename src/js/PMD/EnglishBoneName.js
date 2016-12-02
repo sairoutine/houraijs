6b65d99dc934702f998793d8e1eb4f64e03636b7
sairoutine <sairoutine@gmail.com>
2016-12-03 00:15:34 +0900

    update EnglishBoneName class

--- src/js/PMD/EnglishBoneName.js
+++ src/js/PMD/EnglishBoneName.js
@@ -1,16 +1,21 @@
 'use strict';
 
 function PMDEnglishBoneName(id) {
-  this.id = id;
-  this.name = null;
+	this.id = id;
+	this.name = null;
 }
+PMDEnglishBoneName.STRUCTURE = {
+	name: {type: 'char', isArray: true, size: 20}
+};
 
 
+/*
 PMDEnglishBoneName.prototype.dump = function() {
   var str = '';
   str += 'id: '   + this.id   + '\n';
   str += 'name: ' + this.name + '\n';
   return str;
 };
+*/
 
 module.exports = PMDEnglishBoneName;
