21589bae23c695352c5e577927f35ca9369fe12a
sairoutine <sairoutine@gmail.com>
2016-12-03 00:54:33 +0900

    add: PMDVertex class

---
+++ src/js/PMD/Vertex.js
@@ -0,0 +1,47 @@
+'use strict';
+function PMDVertex(id) {
+	this.id = id;
+	this.position = null;
+	this.normal = null;
+	this.uv = null;
+	this.boneIndices = null;
+	this.boneWeight = null;
+	this.edgeFlag = null;
+
+	//this.boneWeightFloat1 = null;
+	//this.boneWeightFloat2 = null;
+}
+PMDVertex.STRUCTURE = {
+	position: {type: 'float', isArray: true, size: 3},
+	normal: {type: 'float', isArray: true, size: 3},
+	uv: {type: 'float', isArray: true, size: 2},
+	boneIndices: {type: 'uint16', isArray: true, size: 2},
+	boneWeight: {type: 'uint8'},
+	edgeFlag: {type: 'uint8'}
+};
+/*
+PMDVertex.prototype.setup = function() {
+  this.boneWeightFloat1 = this.boneWeight/100;
+  this.boneWeightFloat2 = (100-this.boneWeight)/100;
+};
+
+
+PMDVertex.prototype.dump = function() {
+  var str = '';
+  str += 'id: '          + this.id          + '\n';
+  str += 'position: '    + this.position    + '\n';
+  str += 'normal: '      + this.normal      + '\n';
+  str += 'uv: '          + this.uv          + '\n';
+  str += 'boneIndices: ' + this.boneIndices + '\n';
+  str += 'boneWeight: '  + this.boneWeight  + '\n';
+  str += 'edgeFlag: '    + this.edgeFlag    + '\n';
+  return str;
+};
+
+
+PMDVertex.prototype.toRight = function() {
+  this.position[2] = -this.position[2];
+  this.normal[2] = -this.normal[2];
+};
+*/
+module.exports = PMDVertex;
