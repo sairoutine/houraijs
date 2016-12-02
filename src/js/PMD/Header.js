4e844d60f1e70bdd50af67d6b05f3ca0395b58d3
sairoutine <sairoutine@gmail.com>
2016-12-03 00:37:02 +0900

    update: PMDHeader class

--- src/js/PMD/Header.js
+++ src/js/PMD/Header.js
@@ -5,6 +5,13 @@ function PMDHeader() {
 	this.modelName = null;
 	this.comment = null;
 }
+PMDHeader.HEADER_STRUCTURE = {
+	magic: {type: 'char', isArray: true, size: 3},
+	version: {type: 'float'},
+	modelName: {type: 'char', isArray: true, size: 20},
+	comment: {type: 'char', isArray: true, size: 256}
+};
+
 
 
 PMDHeader.prototype.valid = function() {
