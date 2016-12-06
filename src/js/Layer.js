8ac12dd929527be2eb5fec748f7cad62808845bb
sairoutine <sairoutine@gmail.com>
2016-12-06 16:23:35 +0900

    add: _initWebGL method

--- src/js/Layer.js
+++ src/js/Layer.js
@@ -3,8 +3,34 @@
 /* WebGL layer */
 var Layer = function(canvas) {
 	this.canvas = canvas;
+	this.gl = this._initWebGL(canvas);
 
 };
+
+
+Layer.prototype._initWebGL = function(canvas) {
+	var NAMES = ['webgl', 'experimental-webgl'];
+	var context = null;
+
+	for (var i = 0; i < NAMES.length; i++) {
+		try {
+			context = canvas.getContext(NAMES[i], {antialias: true});
+
+		}
+		catch(e) {}
+	}
+
+	if(context) {
+		context.viewportWidth = canvas.width;
+		context.viewportHeight = canvas.height;
+	}
+	else {
+		window.alert("Failed to get WebGL context.");
+	}
+
+	return context;
+};
+
 
 
 module.exports = Layer;
